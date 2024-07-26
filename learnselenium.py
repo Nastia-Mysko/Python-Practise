621ca5a47c431e7e0349142be4d6e6c2fec88b94
Enjey <enjey@MacBook-Pro-13.local>
2024-07-26 15:17:48 +0300

    updatefile

--- learnselenium.py
+++ learnselenium.py
@@ -1,7 +1,8 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 
 
@@ -23,4 +24,17 @@ for link in links:
     if inner_html and "МОЇ КУРСИ" in inner_html:
         link.click()
         break
+time.sleep(10)
+
+username_field = driver.find_element(By.ID, "username")
+password_field = driver.find_element(By.ID, "password")
+login_button = driver.find_element(By.ID,"loginbtn")
+
+
+
+username_field.send_keys("a.mysko17@gmail.com")
+time.sleep(10)
+password_field.send_keys("Durex1711!!")
+
+login_button.click()
 time.sleep(10)
